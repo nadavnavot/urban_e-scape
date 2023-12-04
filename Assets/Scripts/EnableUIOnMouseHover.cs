d44cf7ad36f582f3729ea5da8f1c0e61abf716b6
evadinaa <125021803+evadinaa@users.noreply.github.com>
2023-12-04 13:37:50 +0000

    load scene and fog effect

--- Assets/Scripts/EnableUIOnMouseHover.cs
+++ Assets/Scripts/EnableUIOnMouseHover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EnableUIOnMouseHover : MonoBehaviour
 {
@@ -29,12 +30,25 @@ public class EnableUIOnMouseHover : MonoBehaviour
         {
             // Enable the UI element when the mouse is over the target object
             uiElementToEnable.SetActive(true);
+    
+            // Check if the left mouse button is clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Call the ReloadLevel method with the desired scene index (1 for scene 1)
+                ReloadLevel(1);
+            }
         }
         else
         {
             // Disable the UI element when the mouse is not over the target object
             uiElementToEnable.SetActive(false);
         }
+    }
+    
+    void ReloadLevel(int sceneIndex)
+
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 
     void OnMouseEnter()
