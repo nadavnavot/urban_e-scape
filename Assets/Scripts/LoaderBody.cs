d95809857865636fd711898df62f0154714a7aed
evadinaa <125021803+evadinaa@users.noreply.github.com>
2023-12-31 11:37:18 +0000

    fade in/out effect added

---
+++ Assets/Scripts/LoaderBody.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderBody : MonoBehaviour
+{
+   public Animator transition;
+   public float transitionTime = 1f;
+
+   private void Update()
+   {
+       if (Input.GetMouseButtonDown(0))
+       {
+           LoadNextScene();
+       }
+   }
+
+   public void LoadNextScene()
+   {
+       StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+   }
+
+   IEnumerator LoadScene(int sceneIndex)
+   {
+       transition.SetTrigger("Start");
+       yield return new WaitForSeconds(transitionTime);
+       SceneManager.LoadScene(sceneIndex);
+   }
+}
